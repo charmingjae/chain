7b5e9d0627bad1fa32b80ec5bc81b8d7f3f3daee
chaminjae <beingeskimo@gmail.com>
2020-10-02 13:51:30 +0900

    commit

--- src/chain/Wallet.java
+++ src/chain/Wallet.java
@@ -8,4 +8,14 @@ public class Wallet {
 	public Wallet() {
 		generateKeyPair();
 	}
+	
+	public void generateKeyPair() {
+		try {
+			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA","BC");
+			SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+		}
+		catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
