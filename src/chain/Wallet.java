314f89bac562d23b2a7af1b4084d9c7af8477e16
chaminjae <beingeskimo@gmail.com>
2020-10-03 15:10:27 +0900

    commit

--- src/chain/Wallet.java
+++ src/chain/Wallet.java
@@ -1,5 +1,6 @@
 package chain;
 import java.security.*;
+import java.security.spec.ECGenParameterSpec;
 
 public class Wallet {
 	public PrivateKey privateKey;
@@ -13,6 +14,11 @@ public class Wallet {
 		try {
 			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA","BC");
 			SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+			ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+			keyGen.initialize(ecSpec, random);
+			KeyPair keyPair = keyGen.generateKeyPair();
+			privateKey = keyPair.getPrivate();
+			publicKey = keyPair.getPublic();
 		}
 		catch(Exception e) {
 			throw new RuntimeException(e);
