c22b40aeb20a0480e262f34cb44de0c64d6f1421
chaminjae <beingeskimo@gmail.com>
2020-10-04 18:24:37 +0900

    commit

--- src/chain/Transactions.java
+++ src/chain/Transactions.java
@@ -4,5 +4,11 @@ import java.security.*;
 import java.util.ArrayList;
 
 public class Transactions {
-
+	public String transactionId;
+	public PublicKey sender;
+	public PublicKey recipient;
+	public float value;
+	public byte[] signature;
+	
+	public ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
 }
